ec68783c0a377bfbd30f35fb561d659c27852afc
Tam Oh <tamhappy18@gmail.com>
2023-11-18 19:24:57 +0900

    Set2b Ex 4 is solved

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -84,7 +84,9 @@ myGcd x y
 -- * you can compute the length of a string with the length function
 
 leftpad :: String -> Int -> String
-leftpad = todo
+leftpad str n
+  | length str >= n = str
+  | otherwise       = leftpad (" " ++ str) n
 
 ------------------------------------------------------------------------------
 -- Ex 5: let's make a countdown for a rocket! Given a number, you
