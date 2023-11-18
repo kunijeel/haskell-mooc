90548e215ea796c43f9416fc1e7897f45d934795
Tam Oh <tamhappy18@gmail.com>
2023-11-18 19:21:07 +0900

    Set2a Ex 11 is solved

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -169,4 +169,6 @@ eitherDiv x y
 --   addEithers (Left "boom") (Left "fail") ==> Left "boom"
 
 addEithers :: Either String Int -> Either String Int -> Either String Int
-addEithers a b = todo
+addEithers (Right a) (Right b) = Right (a + b)
+addEithers (Left a) _ = Left a
+addEithers _ (Left a) = Left a
