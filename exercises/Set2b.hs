bb0565115c28c0c8d53e4a27d3692e632bff4bbb
Tam Oh <tamhappy18@gmail.com>
2023-11-18 19:23:37 +0900

    Set2b Ex 2 is solved

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -29,7 +29,10 @@ binomial n k = binomial (n - 1) k + binomial (n - 1) (k - 1)
 --   oddFactorial 6 ==> 5*3*1 ==> 15
 
 oddFactorial :: Integer -> Integer
-oddFactorial = todo
+oddFactorial 1 = 1
+oddFactorial n
+  | not (even n) = n * oddFactorial (n - 1)
+  | even n       = oddFactorial (n - 1)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the Euclidean Algorithm for finding the greatest
