d9357df40f4b426c2210165237cb63da4e0442bd
Tam Oh <tamhappy18@gmail.com>
2023-11-18 19:17:36 +0900

    Set2a Ex 2 is solved

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -33,7 +33,9 @@ years = [1982, 2004, 2020]
 -- Hint! remember the take and drop functions.
 
 takeFinal :: Int -> [a] -> [a]
-takeFinal n xs = todo
+takeFinal n xs
+  | (length xs) <= n = xs
+  | otherwise        = drop ((length xs) - n) xs
 
 ------------------------------------------------------------------------------
 -- Ex 3: Update an element at a certain index in a list. More
